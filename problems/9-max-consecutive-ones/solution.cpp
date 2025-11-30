2205fb344afc2816ff7d617f801ae4449f5d89c2
shreyanshisoor <soorshreyanshi@gmail.com>
2025-11-30 14:46:44 +0530

    add: #9 Max Consecutive Ones Solution README

---
+++ problems/9-max-consecutive-ones/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count = 0, maxCount = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                count++;
+                if (count > maxCount) maxCount = count;
+            } else {
+                count = 0;
+            }
+        }
+        return maxCount;
+    }
+};
